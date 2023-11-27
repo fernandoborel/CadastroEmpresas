28ec571030b6333d7430beb3df466636351019c3
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2023-11-27 13:03:11 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'projetoempresas';
+  constructor(
+    private httpClient: HttpClient //injeção de dependência
+  ) {}
+
+  //formulário
+  formRegister = new FormGroup({
+    //campo do formulário
+    nome: new FormControl('', [
+      //regras de validação
+      Validators.required,
+    ]),
+
+    //campo do formulário
+    email: new FormControl('', [
+      //regras de validação
+      Validators.required,
+      Validators.email,
+    ]),
+
+    //campo do formulário
+    senha: new FormControl('', [
+      //regras de validação
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+    ]),
+  });
+
+  //função para acessar os campos e validações
+  //do formulário na página HTML
+  get form(): any {
+    return this.formRegister.controls;
+  }
+
+  //função para realizar o cadastro do usuário
+  onSubmit(): void {
+    //requisição para a API..
+    this.httpClient
+      .post(
+        //METHOD POST
+        'http://localhost:5188/api/Register', //ENDPOINT
+        this.formRegister.value //REQUEST DATA
+      )
+      .subscribe(
+        //RESPOSTA
+        (data: any) => {
+          //mensagem obtida da API..
+          alert(data.message);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
