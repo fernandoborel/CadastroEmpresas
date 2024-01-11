0a4944b6cc78f35744f98346bbac07d7a32b3f76
fernandoborel <fernandomenezesborel@gmail.com>
2024-01-11 16:29:33 -0300

    feat. exclusão funcionando

--- src/app/components/empresas/empresas-consulta/empresas-consulta.component.ts
+++ src/app/components/empresas/empresas-consulta/empresas-consulta.component.ts
@@ -14,6 +14,8 @@ export class EmpresasConsultaComponent {
   pagina: number = 1; //paginação
   filtro: any = {nomeFantasia: ''}; //filtro
 
+  mensagem: string = '';
+
   constructor(
     private httpClient: HttpClient,
     private spinner: NgxSpinnerService
@@ -30,6 +32,29 @@ export class EmpresasConsultaComponent {
           this.spinner.hide();
         }
       );
+  }
+
+  onDelete(idEmpresa: string): void{
+    if(window.confirm('Deseja realmente excluir a empresa selecionada?')){
+
+      this.spinner.show();
+
+      this.httpClient.delete(environment.apiUrl + "/Empresas/" + idEmpresa)
+        .subscribe({
+          next: (res: any) => {
+            this.mensagem = `Empresa ${res.nomeFantasia} excluída com sucesso!`;
+            
+            this.ngOnInit();
+            this.pagina = 1;
+            this.filtro.nomeFantasia = '';
+            this.spinner.hide();
+          },
+          error: (e)=>{
+            console.log(e)
+            this.spinner.hide();
+          }
+        })
+    }
   }
 
   //paginação do componente
