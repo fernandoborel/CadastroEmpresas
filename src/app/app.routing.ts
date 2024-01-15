349348ccf33239dd1f74b59ada3d0fe46d1db016
fernandoborel <fernandomenezesborel@gmail.com>
2024-01-15 12:33:09 -0300

    fix. crud funcionando

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticationGuard } from "./guards/authentication.guard";
 
-//importando os componentes que serão mapeados
+//importando os componente que serão mapeados
 import { UserLoginComponent } from "./components/users/user-login/user-login.component";
 import { UserRegisterComponent } from "./components/users/user-register/user-register.component";
 import { UserPasswordComponent } from "./components/users/user-password/user-password.component";
-
 import { EmpresasCadastroComponent } from "./components/empresas/empresas-cadastro/empresas-cadastro.component";
 import { EmpresasConsultaComponent } from "./components/empresas/empresas-consulta/empresas-consulta.component";
 import { EmpresasEdicaoComponent } from "./components/empresas/empresas-edicao/empresas-edicao.component";
-
 import { FuncionariosCadastroComponent } from "./components/funcionarios/funcionarios-cadastro/funcionarios-cadastro.component";
 import { FuncionariosConsultaComponent } from "./components/funcionarios/funcionarios-consulta/funcionarios-consulta.component";
 import { FuncionariosEdicaoComponent } from "./components/funcionarios/funcionarios-edicao/funcionarios-edicao.component";
@@ -19,15 +17,13 @@ import { FuncionariosEdicaoComponent } from "./components/funcionarios/funcionar
 const routes: Routes = [
     { path: '', component: UserLoginComponent }, /* raiz do projeto */
     { path: 'user-register', component: UserRegisterComponent },
-    { path: 'user-password', component: UserPasswordComponent},
-
-    { path: 'empresas-cadastro', component: EmpresasCadastroComponent, canActivate: [AuthenticationGuard]},
-    { path: 'empresas-consulta', component: EmpresasConsultaComponent, canActivate: [AuthenticationGuard]},
-    { path: 'empresas-edicao/:idEmpresa', component: EmpresasEdicaoComponent, canActivate: [AuthenticationGuard]},
-
-    {path: 'funcionarios-cadastro', component: FuncionariosCadastroComponent, canActivate: [AuthenticationGuard]},
-    {path: 'funcionarios-consulta', component: FuncionariosConsultaComponent, canActivate: [AuthenticationGuard]},
-    {path: 'funcionarios-edicao/idFuncionario', component: FuncionariosEdicaoComponent, canActivate: [AuthenticationGuard]}
+    { path: 'user-password', component: UserPasswordComponent },
+    { path: 'empresas-cadastro', component: EmpresasCadastroComponent, canActivate: [AuthenticationGuard] },
+    { path: 'empresas-consulta', component: EmpresasConsultaComponent, canActivate: [AuthenticationGuard] },
+    { path: 'empresas-edicao/:idEmpresa', component: EmpresasEdicaoComponent, canActivate: [AuthenticationGuard] },
+    { path: 'funcionarios-cadastro', component: FuncionariosCadastroComponent, canActivate: [AuthenticationGuard] },
+    { path: 'funcionarios-consulta', component: FuncionariosConsultaComponent, canActivate: [AuthenticationGuard] },
+    { path: 'funcionarios-edicao/:idFuncionario', component: FuncionariosEdicaoComponent, canActivate: [AuthenticationGuard] }
 ];
 
 //registrando as rotas e o módulo
